730872aa0159a315b3187a626c13fb6745339f75
meirryy <mei260019205@gmail.com>
2025-07-16 01:00:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const map = L.map('map').setView([42.2808, -83.7430], 13); // Example: Ann Arbor, MI
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 19
-}).addTo(map);
+const map = L.map('map', {
+  zoomControl: false,
+  attributionControl: false
+}).setView([42.2808, -83.7430], 13);
