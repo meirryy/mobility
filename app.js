caaac4e8f4cc4bf214817ece4be7b9c5b44b4604
meirryy <mei260019205@gmail.com>
2025-07-16 00:58:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const map = L.map('map').setView([42.2808, -83.7430], 13); // Example: Ann Arbor, MI
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data © OpenStreetMap contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
 }).addTo(map);
