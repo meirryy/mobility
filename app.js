b463de0a67c88589498e56c0bf80325e8b4b1d7e
meirryy <mei260019205@gmail.com>
2025-07-16 01:34:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   detectRetina: true  
 }).addTo(map);
 
-let map = L.map('map', {
-  zoomControl: false,
-  attributionControl: true
-}).setView([42.2808, -83.7430], 13);
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 19,
-  detectRetina: true
-}).addTo(map);
-
 let routeLayers = [];
 
 async function geocode(address) {
